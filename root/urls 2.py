f6ba97635db4af02c46b7ddb44ae1b136d4dee39
Chytoe <tochyegeonu97@gmail.com>
2021-12-31 12:32:33 -0600

    Deleted copy files

--- root/urls 2.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework_jwt.views import obtain_jwt_token
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('auth/', obtain_jwt_token),
-]
