5f97de138a3e23784cda28e68550f0da58ec8d1d
JonathanBanda <jonathanbanda00@gmail.com>
2022-02-12 20:41:26 -0600

    fixed the image bug and cleaned up item

--- core/models.py
+++ core/models.py
@@ -5,16 +5,19 @@ from django.contrib.postgres.fields import ArrayField
 from django.forms import IntegerField
 from django_mysql.models import ListTextField
 
+
+def upload_to(instance, filename):
+    return 'itemImages/{filename}'.format(filename=filename)
+
 class Item(models.Model):
     name = models.CharField(max_length=50)
     id = models.AutoField(primary_key=True)
     type = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
-    total = models.IntegerField(default=1)
+    total = models.IntegerField(default=0)
     out = models.IntegerField(default=0)
-    available = models.IntegerField(default=1)
+    available = models.IntegerField(default=0)
     ser_no = models.CharField(max_length=255)
-    image = models.ImageField(null=False, blank=False)
 
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
@@ -25,7 +28,6 @@ class Account(models.Model):
 
 class Instruct(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    # sections = ArrayField(models.IntegerField(default=1))
     sections = ListTextField(base_field=models.IntegerField(), size=100,)
     classes = ListTextField(base_field=models.IntegerField(), size=100,)
 
@@ -34,5 +36,6 @@ class Classes(models.Model):
     instructor = models.IntegerField()
     sections = ListTextField(base_field=models.IntegerField(), size=10, max_length=(10 * 5), blank=True, null=True)
     number = models.IntegerField()
+
 
     
